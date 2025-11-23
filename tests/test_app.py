ef4d20a8d4def45fe56e4fe631701c3acf1674ee
Siddharth <sgajbhiye96@gmail.com>
2025-11-23 14:11:37 +0530

    updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
-# ...existing code...
+
+import sys
+import os
+
+# Fix for GitHub Actions to find app.py
+sys.path.append(os.getcwd())
+
 from app import app
 
 client = app.test_client()
@@ -16,13 +22,10 @@ def test_create_todo():
 def test_get_todos():
     res = client.get("/todo")
     assert res.status_code == 200
-
 def test_update_todo():
     data = {"task": "Updated task"}
     res = client.put("/todo/0", json=data)
     assert res.status_code == 200
-
 def test_delete_todo():
     res = client.delete("/todo/0")
     assert res.status_code == 200
-# ...existing code...
